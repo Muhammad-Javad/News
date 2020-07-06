035eb88b4c5668cbb57068a7b3d3014cac121590
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-07-07 02:34:16 +0430

    build FavoriteFragment.kt

---
+++ app/src/main/java/com/javadsh98/news/ui/main/fragment/favorite/FavoriteAdapter.kt
@@ -0,0 +1,79 @@
+package com.javadsh98.news.ui.main.fragment.favorite
+
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import coil.size.Precision
+import coil.size.Scale
+import com.javadsh98.news.R
+import com.javadsh98.news.common.rawDateToPretty
+import com.javadsh98.news.data.model.Article
+import kotlinx.android.synthetic.main.item_article.view.*
+import org.ocpsoft.prettytime.PrettyTime
+
+typealias OnItemClickListener = (Article) -> Unit
+
+class FavoriteAdapter(val onItemClickListener: OnItemClickListener) : ListAdapter<Article, FavoriteViewHolder>(diff) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        return FavoriteViewHolder.create(parent, onItemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    companion object {
+        val diff = object : DiffUtil.ItemCallback<Article>() {
+            override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return TextUtils.equals(oldItem.urlToImage, newItem.urlToImage)
+            }
+
+            override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+class FavoriteViewHolder(itemview: View, val onItemClickListener: OnItemClickListener) : RecyclerView.ViewHolder(itemview) {
+
+    fun onBind(article: Article) {
+        if (article.urlToImage != null)
+            itemView.imageview_item_image
+                .load(article.urlToImage){
+                    placeholder(R.drawable.img_item_preload)
+                    error(R.drawable.img_item_preload)
+                    precision(Precision.EXACT)
+                    scale(Scale.FILL)
+                }
+        itemView.textview_item_title.text = "${article.title}"
+        itemView.textview_item_date.text = "${rawDateToPretty(article.publishedAt)}"
+        itemView.textvew_item_author.text = "${article.author ?: ""}"
+
+        //listener
+        itemView.cardview_item.setOnClickListener {
+            onItemClickListener.invoke(article)
+        }
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, onItemClickListener: OnItemClickListener): FavoriteViewHolder {
+            return FavoriteViewHolder(
+                LayoutInflater
+                    .from(parent.context).inflate(
+                        R.layout.item_article
+                        , parent
+                        , false
+                    )
+                , onItemClickListener
+            )
+        }
+    }
+}
--- app/src/main/java/com/javadsh98/news/ui/main/fragment/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/javadsh98/news/ui/main/fragment/favorite/FavoriteFragment.kt
@@ -1,60 +1,63 @@
 package com.javadsh98.news.ui.main.fragment.favorite
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.javadsh98.news.R
+import com.javadsh98.news.common.hide
+import com.javadsh98.news.common.show
+import com.javadsh98.news.ui.main.fragment.home.HomeFragmentDirections
+import kotlinx.android.synthetic.main.fragment_favorite.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavoriteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FavoriteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    val viewmodel: FavoriteViewModel by viewModel()
+    lateinit var adapter: FavoriteAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
+        setupRecyclerview()
+        getFavorites()
+
+    }
+
+    private fun getFavorites() {
+        viewmodel.favorites.observe(viewLifecycleOwner, Observer {
+            if(it.isEmpty()){
+                textview_favorite_empty_list.show()
+            }else {
+                textview_favorite_empty_list.hide()
+            }
+            adapter.submitList(it)
+        })
+    }
+
+    private fun setupRecyclerview() {
+        recyclerview_favorite.setHasFixedSize(true)
+        recyclerview_favorite.layoutManager = LinearLayoutManager(requireContext())
+        adapter = FavoriteAdapter {
+            val action = FavoriteFragmentDirections.actionFavoriteFragmentToDetailFragment(it, it.title)
+            findNavController().navigate(action)
+        }
+        recyclerview_favorite.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_favorite, menu)
+        val deleteAll = menu.findItem(R.id.menu_delete_all)
+        deleteAll.setOnMenuItemClickListener {
+            viewmodel.deleteAll()
+            false
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavoriteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavoriteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
